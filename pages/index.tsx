69222d98e92d83f06c71c717405f38a5b03ac492
FirstBright <mipo8890@gmail.com>
2025-04-24 11:47:35 +0900

    add og image

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,18 @@ export default function Home() {
                     name='description'
                     content='An anonymous community map that easily finds good places around me'
                 />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://overcome0.be/" />
+                <meta property="og:title" content="GoodMap - Find and share good hidden places" />
+                <meta property="og:description" content="An anonymous community map that easily finds good places around me" />
                 <meta property="og:image" content="https://overcome0.be/og/goodmap.webp" />
+
+
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://overcome0.be/" />
+                <meta property="twitter:title" content="GoodMap - Find and share good hidden places" />
+                <meta property="twitter:description" content="An anonymous community map that easily finds good places around me" />
+                <meta property="twitter:image" content="https://overcome0.be/og/goodmap.webp" />
             </Head>
             <main className='h-screen'>
                 <MapComponent />
