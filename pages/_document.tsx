c439ac7db8743396c2d2dbdb76e960596b279cbc
FirstBright <mipo8890@gmail.com>
2025-06-11 18:01:06 +0900

    change Script to script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 export default function Document() {
   const isProd = process.env.NODE_ENV === "production";
 
@@ -7,12 +6,11 @@ export default function Document() {
     <Html lang="en">
       <Head>
       {isProd && (
-          <Script
+          <script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9025940068718161"
-            strategy="lazyOnload"
             crossOrigin="anonymous"
-          />
+          ></script>
         )}
         </Head>
       <body className="antialiased">
