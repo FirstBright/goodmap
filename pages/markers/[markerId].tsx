72462bfbb6364338cd7e625af49b4321bb8773fa
FirstBright <mipo8890@gmail.com>
2025-06-13 19:49:36 +0900

    delete loading message

--- pages/markers/[markerId].tsx
+++ pages/markers/[markerId].tsx
@@ -10,10 +10,7 @@ const MapComponent = dynamic(() => import("@/components/MapComponent"), {
     ssr: false,
     loading: () => (
         <div className='h-screen flex flex-col items-center justify-center bg-gray-100'>
-            <div className='w-full max-w-[1080px] px-4 sm:px-6 text-center'>
-                <h1 className='text-3xl font-bold text-gray-800 mb-4'>
-                    Loading Map...
-                </h1>
+            <div className='w-full max-w-[1080px] px-4 sm:px-6 text-center'>                
                 <p className='text-lg text-gray-600 mb-6'>
                     Explore posts and share experiences at this location on GoodMap.
                 </p>
